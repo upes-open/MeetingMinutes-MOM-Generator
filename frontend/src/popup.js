9c30ae8ab7bcf956f3040117d5a81086bbb82144
Savvy48 <viraj.patel482@gmail.com>
2023-08-07 21:17:47 +0530

    Added bootstrap files and get users functionality
    Added bootstrap and get users functionality

--- frontend/src/popup.js
+++ frontend/src/popup.js
@@ -1,8 +1,31 @@
-let listenBtn = document.querySelector(".listen-btn")
+document.addEventListener('DOMContentLoaded', function () {
 
-listenBtn.addEventListener('click', function () {
-    if (listenBtn.innerText == "Start Listening")
-        listenBtn.innerText = "Stop Listening"
-    else
-        listenBtn.innerText = "Start Listening"
-})
+    let listenBtn = document.querySelector('.listen-btn');
+    let playBtn = document.querySelector('#playButton');
+    let downloadBtn = document.querySelector('.download-btn');
+
+    listenBtn.addEventListener('click', function () {
+        if (this.innerText == "Start Listening") {
+            chrome.runtime.sendMessage({
+                message: 'startListening'
+            });
+        } else {
+            chrome.runtime.sendMessage({
+                message: 'stopListening'
+            });
+        }
+    });
+
+    playBtn.addEventListener('click', function () {
+        chrome.runtime.sendMessage({
+            message: 'playAudio'
+        });
+    });
+
+    downloadBtn.addEventListener('click', function () {
+        chrome.runtime.sendMessage({
+            message: 'downloadSummary'
+        });
+    });
+    
+});
