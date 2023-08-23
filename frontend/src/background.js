c0ec596caf83402c774af6ef5a6a5d6de09ef4c6
Savvy48 <viraj.patel482@gmail.com>
2023-08-23 23:50:18 +0530

    added audio recording functionality

--- frontend/src/background.js
+++ frontend/src/background.js
@@ -1,15 +1,40 @@
+let recording;
+chrome.storage.local.get('recording', (result) => {
+    if ('recording' in result) {
+        recording = result.recording;
+    }
+    else recording = false;
+});
+
 chrome.runtime.onMessage.addListener(
     function (request) {
+        if (request.message === 'startListening') {
+            if (!recording) {
+                startRecording();
+            } else {
+                console.log("Recording is already active.");
+            }
+        }
+
+        if (request.message === 'stopListening') {
+            if (recording) {
+                stopRecording();
+            }
+            else {
+                console.log("Recording is not active.");
+            }
+        }
+
         if (request.message === 'playAudio') {
-            sendMessageToContentScript('playAudio');
+            sendMessageToContentScript({ message: 'playAudio' });
         }
 
         if (request.message === 'reqAttendees') {
-            sendMessageToContentScript({message: 'getAttendees'});
+            sendMessageToContentScript({ message: 'getAttendees' });
         }
 
         if (request.message === 'downloadSummary') {
-            sendMessageToContentScript({message: 'getSummary'});
+            sendMessageToContentScript({ message: 'getSummary' });
         }
     }
 );
@@ -25,4 +50,52 @@ function sendMessageToContentScript(message, value) {
             console.error('No active tab found.');
         }
     });
+}
+
+function startRecording() {
+    chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
+        const tab = tabs[0];
+        if (tab) {
+
+            const existingContexts = await chrome.runtime.getContexts({});
+            const offscreenDocument = existingContexts.find(
+                (c) => c.contextType === 'OFFSCREEN_DOCUMENT'
+            );
+
+            if (!offscreenDocument) {
+                await chrome.offscreen.createDocument({
+                    url: 'frontend/src/offscreen.html',
+                    reasons: ['USER_MEDIA'],
+                    justification: 'Recording from chrome.tabCapture API'
+                });
+            }
+
+            const streamId = await chrome.tabCapture.getMediaStreamId({
+                targetTabId: tab.id
+            });
+
+            chrome.runtime.sendMessage({
+                type: 'start-recording',
+                target: 'offscreen',
+                data: streamId
+            });
+            recording = true;
+            chrome.storage.local.set({ recording: true });
+        } else {
+            console.error('No active tab found.');
+        }
+    });
+}
+
+function stopRecording() {
+    if (recording) {
+        chrome.runtime.sendMessage({
+            type: 'stop-recording',
+            target: 'offscreen'
+        });
+        recording = false;
+        chrome.storage.local.set({ recording: false });
+    } else {
+        console.log("Recording is not active.");
+    }
 }
